bbc7d71a648ba550b612150a27f18b252731a67d
Cristobal <cristobal.lobost@gmail.com>
2020-07-16 19:37:42 -0400

    :feat: update Fetch data and update your state

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import News from "./News/News";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">My feed</h1>
+        <h1 className="App-title">My feeds</h1>
+        <News />
       </header>
     </div>
   );
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,8 +2,31 @@ import React, { Component } from "react";
 import NewSingle from "./NewSingle";
 
 class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+    };
+  }
+
+  componentDidMount() {
+    const url =
+      "http://newsapi.org/v2/everything?q=bitcoin&from=2020-06-16&sortBy=publishedAt&apiKey=b593f046cacb403781f839c987b2c84d";
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          news: data.articles,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+
   renderItems() {
-    return this.props.items.map((item) => (
+    return this.state.news.map((item) => (
       <NewSingle key={item.id} item={item} />
     ));
   }
