3f6d63ad05e6617aa9230c7f1d67f44551039795
Cristobal <cristobal.lobost@gmail.com>
2020-07-16 22:33:48 -0400

    :hammer: refactor component news [correction]

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import News from "./News/News";
 
-function App() {
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       news1: {
-        type: 'top-headlines',
-        query: 'sources=bbc-news'
+        type: "top-headlines",
+        query: "sources=bbc-news",
       },
       news2: {
-        type: 'everything',
-        query: 'domains=techcrunch.com&language=en'
-      }
+        type: "everything",
+        query: "domains=techcrunch.com&language=en",
+      },
     };
   }
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">My feeds</h1>
-      </header>
-      <News />
-    </div>
-  );
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">My feed</h1>
+        </header>
+        <News news={this.state.news1} />
+        <News news={this.state.news2} />
+      </div>
+    );
+  }
 }
 
 export default App;
